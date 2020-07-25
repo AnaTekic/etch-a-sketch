6b0afdcb93208db8ada55f0351107bb219216a67
ParchedPanda <tekic.ana@gmail.com>
2020-07-25 17:34:09 +0200

    update

--- script.js
+++ script.js
@@ -2,17 +2,44 @@
 // trebaš sredit grid funkcionalnost
 
 const etchContainer = document.querySelector(".etch-container");
+const clear = document.getElementById("clear");
+const changeSize = document.getElementById("size");
+let gridDiv;
 
 
-addEventListener("load", function createGrid() {
-let i;
-for (i=0; i<256; i++) {
-    let gridDiv = document.createElement("div");
-    gridDiv.classList.add ("grid");
-    gridDiv.innerHTML ="halo"
+//create grid dynamically - default value 16x16
+window.addEventListener("load", function createGrid() {
+  let i;
+  for (i=0; i<256; i++) {
+    gridDiv = document.createElement("div");
     etchContainer.appendChild(gridDiv);
-};
+    gridDiv.innerHTML ="cat";
+    
+  };
+});
+    //prompt size question
+  changeSize.addEventListener("click", function (){   
+        gridDiv.classList.remove("color");
+    let question = prompt ("How many squares per side would you like?", "16");
+    
+    //delete previously created divs
+    while (etchContainer.firstChild) {
+        etchContainer.removeChild(etchContainer.firstChild);
+    };
 
+//     //create new propmt divs
+    etchContainer.style.gridTemplateColumns = `repeat(${question}, 1fr)`;
+    etchContainer.style.gridTemplateRows = `repeat(${question}, 1fr)`;
 
-})
-
+  });
+//etching via hovering
+etchContainer.addEventListener('mouseover', function (e) {
+    e.target.classList.add("color"); //NE ZNAM JEL RADI, BAŠ I NE, ODJEDNOM SE OBOJA SVE
+});
+  
+//  clear grid 
+   clear.addEventListener("click", function clear (){    
+gridDiv.classList.remove("color");
+     
+  
+});  
